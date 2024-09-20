81bfd944ba27869a844e04aa0a9b9d755d2db573
Sang Yoon Nam <naptune123@gmail.com>
2024-09-20 11:39:41 +0900

    v1.0.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "578807219"
+    serviceWorkerVersion: "2577470306"
   }
 });
