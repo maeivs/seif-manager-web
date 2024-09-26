605007b976c0a8de643ad08ee4c061177d1cbf56
Sang Yoon Nam <naptune123@gmail.com>
2024-09-26 13:23:18 +0900

    v1.2.2 - 약관 업데이트

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2221987245"
+    serviceWorkerVersion: "1466900223"
   }
 });
